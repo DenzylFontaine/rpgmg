554d093278e2b9a7b4c1b042b4b744ea328bfc21
DenzylFontaine <ddenzfong@gmail.com>
2016-01-20 01:13:50 -0700

    persona added

--- milestone-1.php
+++ milestone-1.php
@@ -1,1 +1,20 @@
 <!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+		<title>Milestone-1</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<p>Steven<br/>
+		Steven is in his twenties, has worked minimum wage jobs but now has a job with a steady salary.<br/>
+		He frequents our site on his cell phone. He is a hip hop fan and visits our site because<br/>
+		post soundcloud links to all the newest music from our diverse group of artists.<br/>
+		<br/>
+		His goal is to find new hip hop music or some new urban clothes.
+		</p>
+		<h2>Use Case</h2>
+		<
+	</body>
+</html>
