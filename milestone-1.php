22019d7988f9657419060fa3daac4226e096ee25
DenzylFontaine <ddenzfong@gmail.com>
2016-01-20 06:21:42 -0700

    added my use case

--- milestone-1.php
+++ milestone-1.php
@@ -9,17 +9,22 @@
 		<h1>Purpose</h1>
 		<p>The purpose of this site is to create a place for fans of various RNG artists to find music and merchandise.
 		</p>
-		<h2>Persona</h2>
-		<p>Steven<br/>
-		Steven is in his twenties, has worked minimum wage jobs but now has a job with a steady salary.<br/>
-		He frequents our site on his cell phone. He is a hip hop fan and visits our site because<br/>
-		post soundcloud links to all of our newest music from our diverse group of artists.<br/>
 		<br/>
-		His goal is to find new hip hop music or some new urban clothes.
-		</p>
-			<h3>Use Case</h3>
-			<p>Steven uses his phone to search our website to search for and possibly buy new clothes on our website,<br/>
-			while listening to our music through our soundcloud link.
-			</p>
+			<h2>Audience</h2>
+			<P>This website wil primarily be visited by teenage to middle aged men and women.<br/>
+				Who are fans of hip hop and RNB looking for quality new music.
+			</P>
+				<h3>Persona</h3>
+				<p>Steven<br/>
+				Steven is in his twenties, has worked minimum wage jobs but now has a job with a steady salary.<br/>
+				He frequents our site on his cell phone. He is a hip hop fan and visits our site because<br/>
+				post soundcloud links to all of our newest music from our diverse group of artists.<br/>
+				<br/>
+				His goal is to find new hip hop music or some new urban clothes.
+				</p>
+					<h4>Use Case</h4>
+					<p>Steven uses his phone to search our website to search for and possibly buy new clothes on our website,<br/>
+				while listening to our music through our soundcloud link.
+				</p>
 	</body>
 </html>
