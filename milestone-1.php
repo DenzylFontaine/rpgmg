96f90c081c35b0d0c456eee25bfed6c222d09f4e
DenzylFontaine <ddenzfong@gmail.com>
2016-01-20 01:24:22 -0700

    persona added

--- milestone-1.php
+++ milestone-1.php
@@ -10,11 +10,13 @@
 		<p>Steven<br/>
 		Steven is in his twenties, has worked minimum wage jobs but now has a job with a steady salary.<br/>
 		He frequents our site on his cell phone. He is a hip hop fan and visits our site because<br/>
-		post soundcloud links to all the newest music from our diverse group of artists.<br/>
+		post soundcloud links to all of our newest music from our diverse group of artists.<br/>
 		<br/>
 		His goal is to find new hip hop music or some new urban clothes.
 		</p>
-		<h2>Use Case</h2>
-		<
+			<h2>Use Case</h2>
+			<p>Steven uses his phone to search our website to search for and possibly buy new clothes on our website,<br/>
+			while listening to our music through our soundcloud link.
+			</p>
 	</body>
 </html>
