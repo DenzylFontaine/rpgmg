6f9c2da41af91313bc202a5e7f5a6bc67fa0e823
DenzylFontaine <ddenzfong@gmail.com>
2016-01-21 22:45:51 -0700

    changed purpose, persona, and use case

--- milestone-1.php
+++ milestone-1.php
@@ -9,7 +9,6 @@
 		<h1>Purpose</h1>
 		<p>The purpose of this site is to collect all the music, video and clothing made by Dylan Huling and RPG Music so it can be easily 		accessible for fans.
 		</p>
-		<br/>
 			<h2>Audience</h2>
 			<p>This website will primarily be visited by teenage to middle aged men and women.<br/>
 				Who are fans of hip hop and RNG looking for quality new music.
@@ -20,18 +19,23 @@
 					Viewers who love creative music that relates to making it in America today are going to to feel right at home on this
 					site.<br/>
 					Viewers that like urban clothing styles, would find some great stuff to wear here at rpgmg.com
-					Steven is one these views<br/>
+					Steven is one these viewers<br/>
 					Steven is in his twenties, has worked minimum wage jobs but now has a job with a steady salary.<br/>
 					He frequents our site on his cell phone. He is a hip hop fan and visits our site because we<br/>
 					post soundcloud links to all of our newest music from our diverse group of artists.<br/>
 					The music brings Steven to the site but the clothes and social media updates is what keeps steven coming back.<br/>
 					these extra features make him feel closer to and more update on what his favorite artist are doing.<br/>
 					<br/>
-					Stevens main goal is to find new hip hop music or some new urban clothes but it's also to kill time. When he just 					needs to watch something to be entertained while he's in a waiting room we want to be the first site steven thinks of.<br/>
-					Some obstacles we may face is gaining notoriety, we will have to build a name for ourselves so that we can become higher on 					the lists of google and other search engines. We want the site to be easy of people to locate.
+					Stevens main goal is to find new hip hop music or some new urban clothes but it's also to kill time.<br/>
+					When he just needs to watch something to be entertained while he's in a waiting room we want to be the first site steven 					thinks of.<br/>
+					Some obstacles we may face is gaining notoriety, we will have to build a name for ourselves so that we can become higher on 					the lists of google and other search engines.<br/>
+					We want the site to be easy of people to locate.
 					</p>
 						<h4>Use Case</h4>
-						<p>Steven uses his phone to browse our website to search for and possibly buy new clothes. While there he decides to 							check if any of the RPG artists will be performing near his location soon. Steven is dissappiont when he realizes that 							none are, but this visit was not a lost cause because he notice we added new music to the site. He decides to listen 							to the new tracks until his doctor calls him into the room.
+						<p>Steven uses his phone to browse our website to search for and possibly buy new clothes.<br/>
+							While there he decides to check if any of the RPG artists will be performing near his location soon.<br/>
+							Steven is dissappiont when he realizes that none are, but this visit was not a lost cause because he notice we added 							new music to the site.<br/>
+							He decides to listen to the new tracks until his doctor calls him into the room.
 						</p>
 	</body>
 </html>
