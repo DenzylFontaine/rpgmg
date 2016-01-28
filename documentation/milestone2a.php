68973ad763c309b17b56a1edd7bea106ae6db139
DenzylFontaine <ddenzfong@gmail.com>
2016-01-28 01:06:07 -0700

    site-map added and wire frame was added as well

--- documentation/milestone2a.php
+++ documentation/milestone2a.php
@@ -12,7 +12,7 @@
 				<li>Jackets</li>
 				<li>Shirts</li>
 			</br>
-			<p>These 2 pages would have images only with some text to display prices and names of the attire. We want fans to buy and wear outr clothing to spread our brand. Its also a way for fans to feel more like "part of the crew" when they wear the same apparel that the artists wear in the shows or videos. </p>
+			<p>These 2 pages would have images only with some text to display prices and names of the attire. We want fans to buy and wear outr clothing to spread our brand. Its also a way for fans to feel more like "part of the crew" when they wear the same apparel that the artists wear in the shows or videos.</p>
 			</li>
 			<ul>Music</ul>
 			<li>Soundcloud- this page would have on site links to Soundcloud so that albums and individual songs can be played on our site. Video links will also be necessary so the latest music videos we make can be added in (maybe via youtube link). This is what we want the users to come to the site for the music. This page must work smoothly and always have updated content.</li>
