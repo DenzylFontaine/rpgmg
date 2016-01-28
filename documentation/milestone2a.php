75d2703ad0449b4d6d375c35ff700aae640af640
DenzylFontaine <ddenzfong@gmail.com>
2016-01-28 00:07:34 -0700

    /vendor added and documentation directory fixed

--- documentation/milestone2a.php
+++ documentation/milestone2a.php
@@ -6,7 +6,16 @@
 	<body>
 		<div>
 			<ul>Artists</ul>
-			<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and   </li>
+			<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and the RPG philosophy.</li>
+			<ul>Clothes</ul>
+			<li>clothes would split into 2 pages.
+				<li>Jackets</li>
+				<li>Shirts</li>
+			</br>
+			<p>These 2 pages would have images only with text to display prices and names of the attire.</p>
+			</li>
+			<ul>Music</ul>
+			<li>Soundcloud - this page would have on site links to Soundcloud so that albums and individual songs can be played on our site. Video links will also be necessary so the latest music videos we make can be added in (maybe via youtube link).</li>
 		</div>
 	</body>
 </html>
