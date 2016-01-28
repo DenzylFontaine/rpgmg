66a64232a9b8db3ac593ae9a472062417be17ec0
DenzylFontaine <ddenzfong@gmail.com>
2016-01-27 23:57:20 -0700

    /vendor added and documentation directory fixed

---
+++ documentation/milestone2a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>milestone-2a</title>
+	</head>
+	<body>
+		<div>
+			<ul>Artists</ul>
+			<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and   </li>
+		</div>
+	</body>
+</html>
