daf967a0d494a027804af7cd5026e8bda9887050
DenzylFontaine <ddenzfong@gmail.com>
2016-01-30 16:33:54 -0700

    fixing the lists

--- documentation/milestone2a.php
+++ documentation/milestone2a.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8"
 		<title>milestone-2a</title>
 	</head>
 	<body>
@@ -8,16 +9,16 @@
 			<ul>Artists</ul>
 			<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and the RPG philosophy. This page is here so that the viewer can get to know the man that they are listening to. </li>
 			<ul>Clothes</ul>
-			<li>clothes would split into 2 pages.
-				<li>Jackets</li>
-				<li>Shirts</li>
+				<li>clothes would split into 2 pages.
+					<li>Jackets</li>
+					<li>Shirts</li>
+				</Li>
 			</br>
 			<p>These 2 pages would have images only with some text to display prices and names of the attire. We want fans to buy and wear outr clothing to spread our brand. Its also a way for fans to feel more like "part of the crew" when they wear the same apparel that the artists wear in the shows or videos. </p>
-			</li>
 			<ul>Music</ul>
 			<li>Soundcloud- this page would have on site links to Soundcloud so that albums and individual songs can be played on our site. Video links will also be necessary so the latest music videos we make can be added in (maybe via youtube link). This is what we want the users to come to the site for the music. This page must work smoothly and always have updated content.</li>
 		</div>
 		</br>
-		<img src="wireframe.svg"
+		<img src="wireframe.svg" />
 	</body>
 </html>
