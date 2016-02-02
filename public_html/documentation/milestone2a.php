11781711768932843331c67affeac9da525fe62d
DenzylFontaine <ddenzfong@gmail.com>
2016-02-02 12:58:39 -0700

    all my lists fixed

--- public_html/documentation/milestone2a.php
+++ public_html/documentation/milestone2a.php
@@ -6,15 +6,22 @@
 	</head>
 	<body>
 		<div>
-			<ul>Artists</ul>
-			<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and the RPG philosophy. This page is here so that the viewer can get to know the man that they are listening to.</li>
-				<ul>Clothes</ul>
-				<li>Jackets</li>
-				<li>Shirts</li>
+				<h3>Artists</h3>
+			<ul>
+				<li>The artist page will consist of just one artist for now. Dylan Huling, from Albuquerque New Mexico. This page will include a short bio, images of the artist, and the RPG philosophy. This page is here so that the viewer can get to know the man that they are listening to.
+				</li>
+			</ul>
+				<h3>Clothes</h3>
+				<ul>
+					<li>Jackets</li>
+					<li>Shirts</li>
+				</ul>
 			</br>
 			<p>These 2 pages would have images only with some text to display prices and names of the attire. We want fans to buy and wear outr clothing to spread our brand. Its also a way for fans to feel more like "part of the crew" when they wear the same apparel that the artists wear in the shows or videos. </p>
-			<ul>Music</ul>
-			<li>Soundcloud- this page would have on site links to Soundcloud so that albums and individual songs can be played on our site. Video links will also be necessary so the latest music videos we make can be added in (maybe via youtube link). This is what we want the users to come to the site for the music. This page must work smoothly and always have updated content.</li>
+			<h3>Music</h3>
+			<ul>
+				<li>Soundcloud- this page would have on site links to Soundcloud so that albums and individual songs can be played on our site. Video links will also be necessary so the latest music videos we make can be added in (maybe via youtube link). This is what we want the users to come to the site for the music. This page must work smoothly and always have updated content.</li>
+			</ul>
 		</div>
 		</br>
 			<h2>Site Map</h2>
