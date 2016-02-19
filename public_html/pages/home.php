87ac4c78a1d091c1d5c27deb29c9f2b147cdb78b
DenzylFontaine <ddenzfong@gmail.com>
2016-02-19 11:23:15 -0700

    navbar created for home

--- public_html/pages/home.php
+++ public_html/pages/home.php
@@ -31,6 +31,38 @@
 		<link href="stylesheet.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<header></header>
+		<header>
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">RPG</a>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+								</ul>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Artists <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Dylan Huling</a></li>
+									<li><a href="#">Junior J</a></li>
+									<li><a href="#">King</a></li>
+								</ul>
+							<li class="active"><a href="https://bootcamp-coders.cnm.edu/~dfontaine1/rpgmg/public_html/pages/clothes.php">Gear<span class="sr-only"></a></li>
+							<li><a href="https://bootcamp-coders.cnm.edu/~dfontaine1/rpgmg/public_html/pages/artists.php">Music</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</nav>
+		</header>
 	</body>
 </html>
