07d8035535f58c21fea27c7cd2913c82deb38066
DenzylFontaine <ddenzfong@gmail.com>
2016-03-01 14:26:48 -0700

    starting dylans artist

--- public_html/php/navbar.php
+++ public_html/php/navbar.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
 		<!-- LOAD OUR CUSTOM STYLESHEET HERE!!11! -->
@@ -63,17 +63,3 @@
 				</div>
 			</nav>
 		</header>
-<!--		==========================-->
-<!--		start of the Dylans change-->
-<!--		==========================-->
-		<section>
-			<div class="row">
-				<div class="col-md-6">
-					<p>Dylan Huling</p>
-					<img src="images/"
-				</div>
-			</div>
-		</section>
-	</body>
-</html>
-
