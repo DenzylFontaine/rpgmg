93ec1a3520beff7972d5b20fdeb8da0f8f0e01d5
DenzylFontaine <ddenzfong@gmail.com>
2016-03-03 13:10:06 -0700

    vertical pic added to make the page look more like the finished product

--- public_html/pages/artists/dylan-huling.php
+++ public_html/pages/artists/dylan-huling.php
@@ -77,7 +77,7 @@
 						</br>
 						</br>
 						<h2>Dylan Huling</h2>
-						<img src="../../images/musicdude.jpg" />
+						<img src="../../images/dude.jpg" />
 					</div>
 				</div>
 				<div class="col-md-4" >
