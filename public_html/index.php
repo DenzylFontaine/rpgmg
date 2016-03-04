0d3d8ae1b0885ba95306672be8c519b1e1414a21
DenzylFontaine <ddenzfong@gmail.com>
2016-03-03 19:53:56 -0700

    testing links

--- public_html/index.php
+++ public_html/index.php
@@ -21,7 +21,7 @@ $PAGE_TITLE = "Home";
 							<div class="carousel-caption">
 								<h1>Respect</h1>
 								<p>Click here to go to Dylan's page.</p>
-								<p><a class="btn btn-lg btn-danger" href="/../rpgmg/public_html/artists/dylan-huling.php" role="button">Dylan Huling</a></p>
+								<p><a class="btn btn-lg btn-danger" href="/~dfontaine1/rpgmg/public_html/artists/dylan-huling.php" role="button">Dylan Huling</a></p>
 							</div>
 						</div>
 					</div>
