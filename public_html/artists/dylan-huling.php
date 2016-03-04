e16d5877ab44b4094f3f45819cd5b41d68c72e17
DenzylFontaine <ddenzfong@gmail.com>
2016-03-04 08:32:37 -0700

    trying to fix columns and containers

--- public_html/artists/dylan-huling.php
+++ public_html/artists/dylan-huling.php
@@ -9,26 +9,18 @@ $PAGE_TITLE = "yd";
 		<section class="yd">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-2">
-						</br>
-						</br>
-						</br>
+					<div class="col-md-4" class="p-t-8">
 						<h2>Dylan Huling</h2>
 						<img class="img-responsive" src="../images/dude.jpg" />
 					</div>
-					<div class="col-md-8">
+					<div class="col-md-6">
 						</br>
 						<p>And lastly, this one. Checkmate.
 							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.Oh yeah, it's that good. See for yourself.
 							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.First featurette heading. It'll blow your mind.
 							Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-						</br>
 						<iframe width="400px" height="100px" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/245020761&amp;color=cc101d&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-						</br>
-						</br>
 						<iframe width="400px" height="100px" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/229168756&amp;color=cc101d&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-						</br>
-						</br>
 						<a class="twitter-timeline" href="https://twitter.com/ddenz24" data-widget-id="705497830304063489">Tweets by @ddenz24</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 						</div>
 					</div>
