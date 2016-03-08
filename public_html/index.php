eba659a912925e32b6f589f4d24a2f4b2a4a1149
DenzylFontaine <ddenzfong@gmail.com>
2016-03-08 09:20:51 -0700

    new footer added

--- public_html/index.php
+++ public_html/index.php
@@ -138,16 +138,22 @@ $PAGE_TITLE = "Home";
 		<footer class="footer">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-4 text-muted">
-						<p>created by Denzyl Fontaine</p>
+					<div class="col-md-4">
+						<div class="text-muted">
+							<p>created by Denzyl Fontaine</p>
+						</div>
 					</div>
-					<div class="col-md-4 text-muted">
-						<p>RPG Music</p>
-						<p>"Own Yourself"-RPG</p>
+					<div class="col-md-4">
+						<div class="text-muted">
+							<p>RPG Music</p>
+							<p>"Own Yourself"-RPG</p>
+						</div>
 					</div>
-					<div class="col-md-4 text-muted">
-						<p>Contact Us</p>
-						<p>For booking and show purposes only</p>
+					<div class="col-md-4">
+						<div class="text-muted">
+							<p>Contact Us</p>
+							<p>For booking and show purposes only</p>
+						</div>
 					</div>
 				</div>
 			</div>
